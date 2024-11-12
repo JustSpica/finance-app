05680c0b720093a8c07639144d846daab6fc3a10
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2024-11-11 23:26:53 -0300

    feat(repositories): add findById method to users repository

--- apps/api/src/repositories/prisma/prisma-users-repository.ts
+++ apps/api/src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,6 @@
-import { prisma } from '@app/lib/prisma-client'
 import { Prisma, User } from '@prisma/client'
+
+import { prisma } from '@app/lib/prisma-client'
 
 import { UsersRepository } from '../@types/users-repository'
 
@@ -15,6 +16,14 @@ export class PrismaUsersRepository implements UsersRepository {
   async findByEmail(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({
       where: { email }
+    })
+
+    return user
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: { id }
     })
 
     return user
