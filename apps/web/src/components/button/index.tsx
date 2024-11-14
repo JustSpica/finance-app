fdfe9bce4c8e62fc36fa443792a8c67f31230b81
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2024-11-14 17:32:29 -0300

    feat(components): create button icon variant and added loading status

--- apps/web/src/components/button/index.tsx
+++ apps/web/src/components/button/index.tsx
@@ -1,11 +1,17 @@
-import { tv, type VariantProps } from 'tailwind-variants'
+import React from 'react'
+import { Loader2 } from 'lucide-react'
 import { twMerge } from 'tailwind-merge'
+import { tv, type VariantProps } from 'tailwind-variants'
 
 const button = tv({
-  base: 'flex items-center justify-center gap-1 rounded-md px-4 py-2 text-sm transition-colors',
+  base: 'flex items-center justify-center gap-1 rounded-md px-4 py-2 text-sm transition-colors disabled:pointer-events-none disabled:opacity-50',
   variants: {
+    group: {
+      icon: 'p-1'
+    },
     variant: {
-      primary: 'bg-white text-black hover:bg-zinc-200'
+      primary: 'bg-white text-black hover:bg-zinc-200',
+      outline: 'text-white ring-1 ring-zinc-800 hover:bg-zinc-800'
     }
   },
   defaultVariants: {
@@ -13,9 +19,34 @@ const button = tv({
   }
 })
 
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+export type ButtonProps = {
+  loading?: boolean
+} & React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof button>
 
-export function Button({ className, ...props }: ButtonProps) {
-  return <button className={twMerge(button(props), className)} {...props} />
-}
+export const Button = React.forwardRef<React.ElementRef<'button'>, ButtonProps>(
+  ({ className, loading, ...props }, ref) => {
+    if (loading) {
+      return (
+        <button
+          ref={ref}
+          className={twMerge(button(props), className)}
+          disabled
+          {...props}
+        >
+          <Loader2 className="animate-spin" size={18} />
+          Aguarde...
+        </button>
+      )
+    }
+
+    return (
+      <button
+        ref={ref}
+        className={twMerge(button(props), className)}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = 'Button'
